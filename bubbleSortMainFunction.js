6b62f0fc3a0107484c80943ff76cd6833cf37971
Allenyzh <allenyangzhenhao@gmail.com>
2024-10-16 16:01:54 -0400

    update

--- bubbleSortMainFunction.js
+++ bubbleSortMainFunction.js
@@ -1,19 +1,19 @@
 let testArr = [5, 3, 8, 4, 564, 2, 1, 85, 13];
 
-function bubbleSort(arr) {
-  let n = arr.length;
+function bubbleSort(array) {
+  let n = array.length;
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
+      if (array[j] > array[j + 1]) {
         console.log(`i = ${i}, j = ${j}`);
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-        console.log(arr);
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        console.log(array);
       }
     }
   }
-  return arr;
+  return array;
 }
 
 console.log(bubbleSort(testArr));
