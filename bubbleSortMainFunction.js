018534ca3e6067dd648fa30916b91cf06ec22cd0
Allenyzh <allenyangzhenhao@gmail.com>
2024-10-16 16:43:29 -0400

    Update bubbleSortMainFunction.js

--- bubbleSortMainFunction.js
+++ bubbleSortMainFunction.js
@@ -1,19 +1,40 @@
-let testArr = [5, 3, 8, 4, 564, 2, 1, 85, 13];
+let testArr = [5,4,3,2,1];
 
-function bubbleSort(array) {
-  let n = array.length;
+function bubbleSort_loopMethod(array, n = array.length) {
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
       if (array[j] > array[j + 1]) {
-        console.log(`i = ${i}, j = ${j}`);
+        // console.log(`i = ${i}, j = ${j}`);
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
-        console.log(array);
+        // console.log(array);
       }
     }
   }
   return array;
 }
 
-console.log(bubbleSort(testArr));
+function bubbleSort_recursiveMethod(array, n = array.length) {
+  if (n == 1) {
+    return;
+  }
+  function oneStep(j) {
+    if (j < n - 1) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+      oneStep(j + 1);
+    }
+  }
+
+  oneStep(0); // 走一步
+
+  bubbleSort_recursiveMethod(array, n - 1);
+}
+
+// console.log(`loopMethod: ${bubbleSort_loopMethod(testArr)}`);
+bubbleSort_recursiveMethod(testArr);
+console.log(`recursiveMethod: ${testArr}`);
