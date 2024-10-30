89f8f031d37d0c93afa6177ed6913211511e2ff5
Allenyzh <allenyangzhenhao@gmail.com>
2024-10-30 09:26:26 -0400

    -

--- 1026-express/server.js
+++ 1026-express/server.js
@@ -26,8 +26,8 @@ app.get("/api/todos/:id", (req, res) => {
       .status(404)
       .json({ msg: `A todolist with the id of ${id} was not found` });
   }
-  console.log(todo);
-  return res.json({ id: 1, title: "todolist One" });
+  console.log(req);
+  return res.json(todo);
 });
 
 // Add a new todo
@@ -42,16 +42,14 @@ app.post("/api/todos", (req, res) => {
   if (
     !newTodo.title ||
     !newTodo.description ||
-    (newTodo.completed !== false && newTodo.completed !== true)
+    typeof newTodo.completed !== "boolean"
   ) {
-    return res
-      .status(400)
-      .json({
-        msg: "Bro, please fill in all fields, or something wrong happeded including not a vaild completed status!!!",
-      });
+    return res.status(400).json({
+      msg: "Bro, please fill in all fields, or something wrong happeded including not a vaild completed status!!!",
+    });
   }
-  todolist.push(newTodo);
 
+  todolist.push(newTodo);
   res.status(201).json(todolist);
 });
 
@@ -66,10 +64,12 @@ app.put("/api/todos/:id", (req, res) => {
       .json({ msg: `A todolist with the id of ${id} was not found` });
   }
 
-  todo.body = req.body.title;
-  todo.body = req.body.description;
-  todo.body = req.body.completed;
-  
+  todo.title = req.body.title;
+  todo.description = req.body.description;
+  todo.completed = req.body.completed;
+
+  console.log(req);
+
   res.status(200).json(todo);
 });
 
@@ -91,19 +91,16 @@ app.delete("/api/todos/:id", (req, res) => {
 
 // Partially update the todolist
 
-app.patch("/api/todos/:id", (req,res)=>{
-    const id = parseInt(req.params.id);
-    const todo = todolist.find((e) => e.id === id);
-    if (!todo) {
-        return res
-          .status(404)
-          .json({ msg: `A todolist with the id of ${id} was not found` });
-      }
-
-    
-})
+app.patch("/api/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todolist.find((e) => e.id === id);
+  if (!todo) {
+    return res
+      .status(404)
+      .json({ msg: `A todolist with the id of ${id} was not found` });
+  }
+});
 
 app.listen(8000, () => console.log(`服务器在端口8000运行`));
-
 
 // as requested
