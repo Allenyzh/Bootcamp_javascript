69cb136b2482047d55ca41079bf727a4e585faf0
Allenyzh <allenyangzhenhao@gmail.com>
2024-11-07 23:33:00 -0500

    -

---
+++ gomoku/game.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  const chessboard = $("#chessboard");
+  let currentPlayer = "X";
+
+  for (let i = 0; i < 400; i++) {
+    $('<div class="cell"></div>').appendTo(chessboard);
+  }
+
+  $(".cell").on("click", function () {
+    // 检查格子是否已被占用
+    if ($(this).text() === "") {
+      $(this).text(currentPlayer); // 显示当前玩家的标记
+      // 切换玩家
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+    }
+  });
+});
